4910957704c7f1a2dd200bfaf0b12aab4fe10a03
2804983329 <2804983329@qq.com>
2020-09-01 00:41:09 +0800

    20200901

--- py3-data_analysis_and-data-mining/源码/sina.py
+++ py3-data_analysis_and-data-mining/源码/sina.py
@@ -9,3 +9,4 @@ def weibo():
     client=APIclient(app_key=APP_KEY,app_secret=APP_SECRET,redirect_url=CALLBACK_URL)#初始化client   refer_url未写
     refer_url=client.get_authorise_url() #通过什么网址输出
     print "refer_url:" + refer_url
+    ---
