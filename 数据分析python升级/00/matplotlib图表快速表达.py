225fbe6102b77bda68c1458faf6778d2194608d3
2804983329 <2804983329@qq.com>
2020-09-03 19:55:01 +0800

    202009031954

---
+++ 数据分析python升级/00/matplotlib图表快速表达.py
@@ -0,0 +1,16 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+%matplotlib inline
+
+sns.set_style("darkgrid")
+sns.set_context("paper")
+#设置风格、尺度
+
+import warnings
+warnings.filterwarnings('ignore')
+#不发出警告
+
+
+#
