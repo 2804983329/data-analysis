04193e9ade830e0ef7baa019db28f5d2ad5d3d8f
2804983329 <2804983329@qq.com>
2020-09-03 23:07:28 +0800

    2020090302

--- 数据分析python升级/00/matplotlib图表快速表达.py
+++ 数据分析python升级/00/matplotlib图表快速表达.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-%matplotlib inline
+#% matplotlib inline   #用在Jupyter notebook中具体作用是当你调用matplotlib.pyplot的绘图函数plot()进行绘图的时候，或者生成一个figure画布的时候
 
 sns.set_style("darkgrid")
 sns.set_context("paper")
@@ -17,21 +17,34 @@ warnings.filterwarnings('ignore')
 
 df = pd.DataFrame(np.random.randn(1000, 4), index=pd.date_range('1/1/2000',periods=1000), columns=list('ABCD'))
 df = df.cumsum()
-df.plot(kind='line',
+df.plot(kind='line',   #bar 柱状图  line折线图
         style = '--',
         alpha = 0.4,
-        use_index = True,
-        rot = 45,
-        grid = True,
-        figsize = (8,4),
+        use_index = True, #是否使用坐标
+        rot = 45,    #是否旋转
+        grid = True, #是否格网图
+        figsize = (8,4),  #图的大小
         title = 'test',
         legend = True,
         subplots = False,
-        colormap = 'Green'
-        )
+        colormap = 'Greens')
 #subplots → 是否将各个列绘制到不同图表，默认False
 #也可以 → plt.plot(df)
 
+
+#散点图 、 气泡图
+
+plt.figure(figsize=(8,6))
+x = np.random.randn(1000)
+y = np.random.randn(1000)
+plt.scatter(x,y,marker='.',
+            s = np.random.randn(1000)*100,
+            cmap = 'Reds',
+            c = y,
+#s:散点的大小
+#c:散点的颜色
+#vmin，vmax：亮度设置，标量
+#cmap：colormap
 
 
 
