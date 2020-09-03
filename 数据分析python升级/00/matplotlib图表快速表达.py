f07310f6127c52a825c9fc4e1550fcb2c9b29de9
2804983329 <2804983329@qq.com>
2020-09-03 20:22:16 +0800

    2020090301

--- 数据分析python升级/00/matplotlib图表快速表达.py
+++ 数据分析python升级/00/matplotlib图表快速表达.py
@@ -13,4 +13,26 @@ warnings.filterwarnings('ignore')
 #不发出警告
 
 
-#
+#折线图
+
+df = pd.DataFrame(np.random.randn(1000, 4), index=pd.date_range('1/1/2000',periods=1000), columns=list('ABCD'))
+df = df.cumsum()
+df.plot(kind='line',
+        style = '--',
+        alpha = 0.4,
+        use_index = True,
+        rot = 45,
+        grid = True,
+        figsize = (8,4),
+        title = 'test',
+        legend = True,
+        subplots = False,
+        colormap = 'Green'
+        )
+#subplots → 是否将各个列绘制到不同图表，默认False
+#也可以 → plt.plot(df)
+
+
+
+
+
